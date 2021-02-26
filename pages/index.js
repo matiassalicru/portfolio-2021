5cc5dbac01be5ed8e127d073225893a36e48ac99
Matias Salicru <mattiassalicru@gmail.com>
2021-02-26 12:02:08 -0300

    added SwiperJS for projects sliders in mobile

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,21 @@ import Hamburger from "components/Icons/Hamburguer";
 import Arrow from "components/Icons/Arrow";
 import home from "styles/Home.module.scss";
 import about from "styles/About.module.scss";
+import skills from "styles/Skills.module.scss";
+import actual from "styles/Actual.module.scss";
+import projects from "styles/Projects.module.scss";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from "swiper";
 
 export default function Home() {
+  SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
   return (
     <div className={home.container}>
       <Head>
@@ -41,6 +54,89 @@ export default function Home() {
             maiores, quisquam suscipit voluptas nobis ullam ipsa ea fuga
             tempora, et soluta. Asperiores a esse corporis explicabo eveniet
             veritatis ea facere.
+          </p>
+          <hr className={about.Vline} />
+        </section>
+        <section className={skills.container}>
+          <hr className={skills.Vline} />
+          <h1 className={skills.title}>What I'm good at</h1>
+          <div className={skills.skillsContainer}>
+            <div className={skills.row}>
+              <p className={skills.skill}>Javascript</p>
+              <p className={skills.skill}>React</p>
+              <p className={skills.skill}>React-Redux</p>
+            </div>
+
+            <div className={skills.row}>
+              <p className={skills.skill}>NextJS</p>
+              <p className={skills.skill}>Figma</p>
+            </div>
+
+            <div className={skills.row}>
+              <p className={skills.skill}>Spanish & english</p>
+            </div>
+          </div>
+          z
+          <hr className={skills.Vline} />
+        </section>
+        <section className={actual.container}>
+          <hr className={actual.Vline} />
+          <h1 className={actual.title}>What I'm in right now</h1>
+
+          <p className={actual.p}>
+            Right now I'm working in my last project, a website for a little
+            store called @darkstar.ar while I'm also in a course about NodeJS.
+          </p>
+
+          <hr className={actual.Vline} />
+        </section>
+        <section className={projects.container}>
+          <h1 className={projects.title}>My projects</h1>
+          <p className={projects.p}>
+            All the experience that I earned, comes from this little projects
+            that I loved to make
+          </p>
+          <Swiper
+            spaceBetween={20}
+            slidesPerView={1}
+            navigation
+            className={projects.swiper}
+            pagination={{ clickable: true }}
+            autoplay={{ delay: 2000 }}
+            scrollbar={{ draggable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+          >
+            <SwiperSlide>
+              <div className={projects.project}>
+                <button>Link</button>
+                <button>repo</button>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={projects.project}>
+                <button>Link</button>
+                <button>repo</button>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={projects.project}>
+                <button>Link</button>
+                <button>repo</button>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={projects.project}>
+                <button>Link</button>
+                <button>repo</button>
+              </div>
+            </SwiperSlide>
+            ...
+          </Swiper>
+
+          <p className={projects.p} style={{marginTop: 40, fontSize: 22}}>
+            Do you have a project in mind? Are you looking for a Front-end
+            developer with experience in React? Do not hesitate to contact me
           </p>
         </section>
       </Layout>
